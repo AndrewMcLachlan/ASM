60179490f18a91a18f89b8da4eb1286ac3f409e8
Andrew McLachlan <amc.home@outlook.com>
2019-03-26 16:42:35 +1000

    Added common test project with SpecFlow helper classes.

---
+++ tests/Asm.Tests.Common/SpecFlowHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace Asm.Tests.Common
+{
+    public static class SpecFlowHelper
+    {
+        public const string NullString = "<NULL>";
+
+        public static string SpecFlowProcess(this string str)
+        {
+            if (str == null) return null;
+            if (str == NullString) return null;
+
+            return str.Replace("\\r", "\r").Replace("\\n", "\n").Replace("\\t", "\t");
+        }
+
+        public static void CatchException(Action action)
+        {
+            var exception = Record.Exception(action);
+
+            Assert.NotNull(ScenarioContext.Current);
+
+            ScenarioContext.Current.Add(CommonSteps.ExceptionKey, exception);
+        }
+    }
+}
