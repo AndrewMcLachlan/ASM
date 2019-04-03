88dc5e844b6a52d5018abc92ccaacfe42b036533
Andrew McLachlan <amc.home@outlook.com>
2019-04-04 08:33:47 +1000

    Added some simple assertion SpecFlow steps that can be shared.

---
+++ src/Asm.Testing/SimpleAssertionSteps.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace Asm.Testing
+{
+    [Binding]
+    public class SimpleAssertionSteps
+    {
+        private readonly ScenarioResult<string> _strResult;
+        private readonly ScenarioResult<int> _intResult;
+        private readonly ScenarioResult<DateTime> _dateResult;
+
+        public SimpleAssertionSteps(ScenarioResult<string> strResult, ScenarioResult<int> intResult, ScenarioResult<DateTime> dateResult)
+        {
+            _strResult = strResult;
+            _intResult = intResult;
+            _dateResult = dateResult;
+        }
+
+        [Then(@"the string value '(.*)' is returned")]
+        public void ThenTheStringValueIsReturned(string expected)
+        {
+            Assert.NotNull(_intResult);
+            Assert.Equal(expected, _strResult.Result);
+        }
+
+        [Then(@"the integer value (.*) is returned")]
+        public void ThenTheIntegerValueIsReturned(int expected)
+        {
+            Assert.NotNull(_intResult);
+            Assert.Equal(expected, _intResult.Result);
+        }
+
+
+        [Then(@"the a date '(.*)' will be returned")]
+        public void ThenTheADateWillBeReturned(DateTime expected)
+        {
+            Assert.NotNull(_dateResult);
+            Assert.Equal(expected, _dateResult.Result);
+        }
+    }
+}
