08ca1c59e80e659b3bf108be9b8575c2d3d871ee
Andrew McLachlan <amc_home@hotmail.com>
2019-04-03 19:11:15 +1000

    Added .NET Standard project for useful Specflow code. Updated Asm.Net.Tests to use Specflow 3 paradigms.

---
+++ src/Asm.Testing/ScenarioData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asm.Testing
+{
+    public class ScenarioData<T>
+    {
+        public T Result { get; set; }
+    }
+}
