c29057de2121d0f6a0cc909b27f73091c77ee703
Andrew McLachlan <amc_home@hotmail.com>
2019-04-03 23:14:45 +1000

    Revert "Changed test projects to .NET Core 2.0."
    This reverts commit 8f4d0ab733f3207ad0d7888c95f174a04577dcca.

---
+++ tests/Asm.Tests.Common/CommonSteps.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+using static Asm.Tests.Common.SpecFlowHelper;
+
+namespace Asm.Tests.Common
+{
+    [Binding]
+    public class CommonSteps
+    {
+        public const string ExceptionKey = "ExpectedException";
+
+        [Then(@"an exception of type '(.*)' is thrown")]
+        public void ThenAnExceptionofTypeIsThrown(string exceptionType)
+        {
+            Type expected = Type.GetType(exceptionType, true);
+
+            var actual = ScenarioContext.Current.Get<Exception>(ExceptionKey);
+
+            Assert.NotNull(actual);
+            Assert.IsType(expected, actual);
+        }
+
+        [Then(@"the exception message is '(.*)'")]
+        public void ThenTheExceptionMessageIs(string message)
+        {
+            var exception = ScenarioContext.Current.Get<Exception>(ExceptionKey);
+
+            Assert.NotNull(exception);
+            Assert.Equal(message.SpecFlowProcess(), exception.Message);
+        }
+
+        [Then(@"the exception parameter name is '(.*)'")]
+        public void ThenTheExceptionParameterNAmeIs(string parameterName)
+        {
+            var exception = ScenarioContext.Current.Get<ArgumentException>(ExceptionKey);
+
+            Assert.NotNull(exception);
+            Assert.Equal(parameterName, exception.ParamName);
+        }
+    }
+}
