38aef37c9cb0a1fb378addb467f67050d68e2b22
Andrew McLachlan <amc_home@hotmail.com>
2019-06-07 21:33:07 +1000

    Added a preprend method to complement the append method. Renamed and simplified sensible case.

--- src/Asm/Extensions/System.String.cs
+++ src/Asm/Extensions/System.String.cs
@@ -35,34 +35,38 @@ namespace Asm.Extensions
         }
 
         /// <summary>
-        /// Converts the string to its most sensible casing.
+        /// Prepends a string to a string, using the separator string.
+        /// </summary>
+        /// <remarks>
+        /// The separator string is not used if the string is empty.
+        /// </remarks>
+        /// <param name="source">The string to prepend to.</param>
+        /// <param name="value">The string to prepend.</param>
+        /// <param name="separator">The string that separates the two strings.</param>
+        /// <returns>The prepended string.</returns>
+        public static string Prepend(this string source, string value, string separator)
+        {
+            if (source.Length == 0)
+            {
+                return value + source;
+            }
+            else
+            {
+                return value + separator + source;
+            }
+        }
+
+        /// <summary>
+        /// Converts the string to title case.
         /// </summary>
         /// <param name="input">The string.</param>
-        /// <returns>A sensibly cased string.</returns>
-        public static string ToSensibleCase(this string input)
+        /// <returns>A title cased string.</returns>
+        public static string ToTitleCase(this string input)
         {
             if (input == null)
             {
                 return null;
             }
-            /*Regex upperCase = new Regex("^[A-Z]+$");
-            Regex lowerCase = new Regex("^[a-z]+$");
-
-            string result = input;
-
-            if (String.IsNullOrEmpty(input))
-            {
-            }
-            else if (upperCase.IsMatch(input) || lowerCase.IsMatch(input))
-            {
-                result = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(input.ToLowerInvariant());
-            }
-            else if (lowerCase.IsMatch(input.Substring(0, 1)))
-            {
-                result = input.Substring(0, 1).ToUpperInvariant() + input.Substring(1);
-            }
-
-            return result;*/
 
             return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(input.ToUpperInvariant());
         }
