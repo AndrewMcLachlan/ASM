76eafffcb1f9d3d84beef6f0fe5c6235eef0568a
Andrew McLachlan <amc_home@hotmail.com>
2019-06-07 21:32:06 +1000

    Created an override for AppendFormatLine that excludes a format provider.

--- src/Asm/Extensions/System.Text.StringBuilder.cs
+++ src/Asm/Extensions/System.Text.StringBuilder.cs
@@ -11,9 +11,9 @@ namespace Asm.Extensions
     public static class StringBuilderExtensions
     {
         /// <summary>
-        /// Appends the string returned by processing a composite format string, which 
-        /// contains zero or more format items, along with the default line terminator, to this instance. Each format item is 
-        /// replaced by the string representation of a corresponding argument in a parameter 
+        /// Appends the string returned by processing a composite format string, which
+        /// contains zero or more format items, along with the default line terminator, to this instance. Each format item is
+        /// replaced by the string representation of a corresponding argument in a parameter
         /// array using a specified format provider.
         /// </summary>
         /// <param name="source">The StringBuilder.</param>
@@ -28,8 +28,27 @@ namespace Asm.Extensions
         /// </returns>
         public static StringBuilder AppendFormatLine(this StringBuilder source, IFormatProvider provider, string format, params object[] args)
         {
-            source.AppendFormat(provider, format, args);
-            return source.AppendLine();
+            return source.AppendFormat(provider, format, args).AppendLine();
+        }
+
+        /// <summary>
+        /// Appends the string returned by processing a composite format string, which
+        /// contains zero or more format items, along with the default line terminator, to this instance. Each format item is
+        /// replaced by the string representation of a corresponding argument in a parameter
+        /// array using a specified format provider.
+        /// </summary>
+        /// <param name="source">The StringBuilder.</param>
+        /// <param name="format">A composite format string.</param>
+        /// <param name="args">An array of objects to format.</param>
+        /// <returns>
+        /// A reference to this instance after the append operation has completed. After
+        /// the append operation, this instance contains any data that existed before
+        /// the operation, suffixed by a copy of format where any format specification
+        /// is replaced by the string representation of the corresponding object argument.
+        /// </returns>
+        public static StringBuilder AppendFormatLine(this StringBuilder source, string format, params object[] args)
+        {
+            return source.AppendFormat(format, args).AppendLine();
         }
     }
 }
