8ed00b8a2c6f43a6d0131abf71c84d2282b2052b
Andrew McLachlan <amc_home@hotmail.com>
2019-06-07 21:33:46 +1000

    Added an ASP.NET Core package.

---
+++ src/Asm.Web.Mvc/TagHelpers/BodyTagHelper.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Asm.Extensions;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Asm.AmCom.Web.TagHelpers
+{
+    public class BodyTagHelper : TagHelper
+    {
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string area = ViewContext.RouteData.Values["area"] as string;
+            string controller = ViewContext.RouteData.Values["controller"] as string;
+            string action = ViewContext.RouteData.Values["action"] as string;
+
+            string className = $"{area} {controller} {action}".ToLowerInvariant();
+
+            output.TagName = "body";
+
+            if (output.Attributes.Any(a => a.Name == "class"))
+            {
+                className = className.Append(output.Attributes["class"].Value as string, " ");
+                output.Attributes.Remove(output.Attributes["class"]);
+            }
+
+            output.Attributes.Add(new TagHelperAttribute("class", className));
+        }
+    }
+}
