bd27fe25704be12ea3ca413424aff83b5ce62322
Andrew McLachlan <amc_home@hotmail.com>
2019-10-27 15:16:32 +1000

    Fixed extensions class to extend service collection.

---
+++ src/Asm.Web.Mvc/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿
+using Asm.Security;
+using Asm.Web.Mvc.Security;
+using Microsoft.AspNetCore.Http;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddPrincipalProvider(this IServiceCollection services)
+        {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IPrincipalProvider, HttpContextPrincipalProvider>();
+        }
+    }
+}
