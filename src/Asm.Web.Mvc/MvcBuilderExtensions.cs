e0cbe14eee0dd6628ada54339e903a458b7743e1
Andrew McLachlan <AMcLachlan@laingorourke.com.au>
2019-10-27 12:23:34 +1000

    Added IPrincipalProvider and web implementation.

---
+++ src/Asm.Web.Mvc/MvcBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿
+using Asm.Security;
+using Asm.Web.Mvc.Security;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Asm.Web.Mvc
+{
+    public static class MvcBuilderExtensions
+    {
+        public static void AddPrincipalProvider(this IMvcBuilder builder)
+        {
+            builder.AddApplicationPart(typeof(MvcBuilderExtensions).Assembly);
+            builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            builder.Services.AddScoped<IPrincipalProvider, HttpContextPrincipalProvider>();
+        }
+    }
+}
---
+++ src/Asm/Security/IPrincipalProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace Asm.Security
+{
+    public interface IPrincipalProvider
+    {
+        ClaimsPrincipal Principal { get; }
+    }
+}
