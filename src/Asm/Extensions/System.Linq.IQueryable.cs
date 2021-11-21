46d9c7fb5c5b27db9864b6a86063878169b534a6
Andrew McLachlan <andy@mclachlan.family>
2021-11-21 21:46:23 +1000

    Added skip/take paging.

---
+++ src/Asm/Extensions/System.Linq.IQueryable.cs
@@ -0,0 +1,9 @@
+﻿namespace Asm.Extensions;
+
+public static class IQueryableExtensions
+{
+    public static IQueryable<T> Page<T>(this IQueryable<T> query, int pageSize, int pageNumber)
+    {
+        return query.Skip((pageNumber - 1) * pageSize).Take(pageSize);
+    }
+}
