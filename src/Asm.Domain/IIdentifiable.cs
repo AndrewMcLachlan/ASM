135e39d008dc5ebe2a6c1e17e01c9f44fa1b754d
Andrew McLachlan <andy@mclachlan.family>
2021-11-21 21:45:54 +1000

    Made IIdentifiable equatable.

--- src/Asm.Domain/IIdentifiable.cs
+++ src/Asm.Domain/IIdentifiable.cs
@@ -1,7 +1,17 @@
-﻿namespace Asm.Domain
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Asm.Domain
 {
-	public interface IIdentifiable<T>
+	public interface IIdentifiable<T> : IEquatable<IIdentifiable<T>>
 	{
+		[NotNull]
 		T Id { get; }
+
+		bool IEquatable<IIdentifiable<T>>.Equals(IIdentifiable<T>? other)
+        {
+			if (other == null) return false;
+
+			return other.Id.Equals(Id);
+        }
 	}
 }
