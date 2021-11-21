6489d86c5f98da7fe6d999a5643f3a480f1e843f
Andrew McLachlan <andy@mclachlan.family>
2021-11-21 21:10:30 +1000

    Added ICollection extensions.

---
+++ src/Asm/Extensions/System.Collections.Generic.ICollection.cs
@@ -0,0 +1,17 @@
+﻿namespace Asm.Extensions;
+
+public static class ICollectionExtensions
+{
+    public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> items)
+    {
+        if (collection == null) throw new ArgumentNullException(nameof(collection));
+        if (collection.IsReadOnly) throw new InvalidOperationException("Collection is readonly");
+
+        if (items == null) return;
+
+        foreach (var item in items)
+        {
+            collection.Add(item);
+        }
+    }
+}
