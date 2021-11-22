3e3313176c42153a2ee1b6eb85fc380311e17173
Andrew McLachlan <andy@mclachlan.family>
2021-11-22 20:17:49 +1000

    Added ColouredConsole.

---
+++ src/Asm/ColouredConsole.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+namespace Asm;
+
+public static class ColouredConsole
+{
+    public static void WriteLine(string value, ConsoleColor colour)
+    {
+        Write(value, colour, Console.WriteLine);
+    }
+
+    public static void Write(string value, ConsoleColor colour)
+    {
+        Write(value, colour, Console.Write);
+    }
+
+    private static void Write(string value, ConsoleColor colour, Action<string> writer)
+    {
+        var currentColor = ForegroundColor;
+        ForegroundColor = colour;
+        writer(value);
+        ForegroundColor = currentColor;
+    }
+}
