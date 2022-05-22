dfb473224880800d769ddde9d0dc6932cd5f2947
Andrew McLachlan <andy@mclachlan.family>
2022-05-22 17:18:59 +1000

    Added null checking to Entity.

--- src/Asm.Domain/Entity.cs
+++ src/Asm.Domain/Entity.cs
@@ -1,14 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Asm.Domain
 {
     public abstract class Entity<TKey> : IIdentifiable<TKey>
     {
         [Key]
+        [NotNull]
         public TKey Id { get; }
 
         protected Entity(TKey id)
         {
+            ArgumentNullException.ThrowIfNull(id);
             Id = id;
         }
 
