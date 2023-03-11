dd6a7d181db850a475a7281e6cf72c94ca9aada7
Andrew McLachlan <andy@mclachlan.family>
2023-03-11 17:43:35 +1000

    Added service collection extensions for unit of work.

---
+++ src/Asm.Domain.Infrastructure/IServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Asm.Domain;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class AsmDomainInfrastructureIServiceCollectionExtensions
+{
+    public static IServiceCollection AddUnitOfWork<T>(this IServiceCollection services) where T : class, IUnitOfWork
+    {
+        return services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<T>());
+    }
+
+}
