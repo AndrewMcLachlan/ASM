67c12c80db8f2c024d422e65bee599147409d354
Andrew McLachlan <andy@mclachlan.family>
2023-03-18 10:01:27 +1000

    Improved WebJobStart.

--- src/Asm.Web/WebJobStart.cs
+++ src/Asm.Web/WebJobStart.cs
@@ -3,39 +3,61 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 
-namespace Asm.Web
+namespace Asm.Web;
+
+public static class WebJobStart
 {
-    public static class WebJobStart
+    public static int Run(string[] args, string appName, Action<HostBuilderContext, IServiceCollection> configureServices, Action<IHost> runner)
     {
-        public static int Run(string[] args, string appName, Action<HostBuilderContext, IServiceCollection> configureServices)
+        Log.Logger = LoggingConfigurator.ConfigureLogging(new LoggerConfiguration(), appName).CreateBootstrapLogger();
+
+        try
         {
-            Log.Logger = LoggingConfigurator.ConfigureLogging(new LoggerConfiguration(), appName).CreateBootstrapLogger();
-
-            try
-            {
-                Log.Information("Starting...");
-                CreateHostBuilder(args, appName, configureServices).UseConsoleLifetime().Build().Run();
-                return 0;
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Fatal host exception");
-                return 1;
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
+            Log.Information("Starting...");
+            var host = CreateHostBuilder(args, appName, configureServices).UseConsoleLifetime().Build();
+            runner(host);
+            return 0;
         }
-
-        public static IHostBuilder CreateHostBuilder(string[] args, string appName, Action<HostBuilderContext, IServiceCollection> configureServices) =>
-            Host.CreateDefaultBuilder(args)
-            .ConfigureAppConfiguration((context, appBuilder) =>
-            {
-                context.HostingEnvironment.ApplicationName = appName;
-            })
-            .UseEnvironment(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"))
-            .UseCustomSerilog()
-            .ConfigureServices(configureServices);
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Fatal host exception");
+            return 1;
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
     }
+
+    public static async Task<int> RunAsync(string[] args, string appName, Action<HostBuilderContext, IServiceCollection> configureServices, Func<IHost, Task> runner)
+    {
+        Log.Logger = LoggingConfigurator.ConfigureLogging(new LoggerConfiguration(), appName).CreateBootstrapLogger();
+
+        try
+        {
+            Log.Information("Starting...");
+            var host = CreateHostBuilder(args, appName, configureServices).UseConsoleLifetime().Build();
+            await runner(host);
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Fatal host exception");
+            return 1;
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args, string appName, Action<HostBuilderContext, IServiceCollection> configureServices) =>
+        Host.CreateDefaultBuilder(args)
+        .ConfigureAppConfiguration((context, appBuilder) =>
+        {
+            context.HostingEnvironment.ApplicationName = appName;
+        })
+        .UseEnvironment(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "LocalDev")
+        .UseCustomSerilog()
+        .ConfigureServices(configureServices);
 }
