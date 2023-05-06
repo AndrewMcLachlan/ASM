78c4a98b66545bf2dbaab715a1294a6077310923
Andrew McLachlan <andy@mclachlan.family>
2023-05-07 08:59:00 +1000

    Removed unnecessary type parameter.

--- src/Asm.Cqrs/Commands/ICommandDispatcher.cs
+++ src/Asm.Cqrs/Commands/ICommandDispatcher.cs
@@ -4,5 +4,5 @@ public interface ICommandDispatcher
 {
     public Task<T> Dispatch<T>(ICommand<T> command, CancellationToken cancellationToken = default);
 
-    public Task Dispatch<T>(ICommand command, CancellationToken cancellationToken = default);
+    public Task Dispatch(ICommand command, CancellationToken cancellationToken = default);
 }
