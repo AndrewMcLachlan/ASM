7a21e03eda7e004b0bf25f1a62d892a52bbba64c
Andrew McLachlan <andy@mclachlan.family>
2023-07-20 20:37:38 +1000

    Added CommandQueryController base class.

---
+++ src/Asm.Web.Api/Controllers/CommandQueryContoller.cs
@@ -0,0 +1,20 @@
+﻿using Asm.Cqrs.Commands;
+using Asm.Cqrs.Queries;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asm.MooBank.Web.Controllers;
+
+[ApiController]
+[Authorize]
+public abstract class CommandQueryController : Controller
+{
+    protected IQueryDispatcher QueryDispatcher { get; private set; }
+    protected ICommandDispatcher CommandDispatcher { get; private set; }
+
+    public CommandQueryController(IQueryDispatcher queryDispatcher, ICommandDispatcher commandDispatcher)
+    {
+        QueryDispatcher = queryDispatcher;
+        CommandDispatcher = commandDispatcher;
+    }
+}
