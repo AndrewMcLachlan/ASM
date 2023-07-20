1b2324e7fe821cd52e358ae8186c8601f1e0460a
Andrew McLachlan <andy@mclachlan.family>
2023-07-20 20:38:37 +1000

    Added modern WebApplicationStart helper method.

---
+++ src/Asm.Web/WebApplicationStart.cs
@@ -0,0 +1,43 @@
+﻿using Asm.Serilog;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace Asm.Web;
+
+public class WebApplicationStart
+{
+    public static int Run(string[] args, string appName, Action<WebApplicationBuilder> addServices, Action<WebApplication> addApp)
+    {
+        Log.Logger = LoggingConfigurator.ConfigureLogging(new LoggerConfiguration(), appName).CreateBootstrapLogger();
+
+
+        try
+        {
+            Log.Information("Starting...");
+
+            var builder = WebApplication.CreateBuilder(new WebApplicationOptions { ApplicationName = appName, Args = args, });
+
+            builder.Host.UseCustomSerilog();
+
+            addServices(builder);
+
+            var application = builder.Build();
+
+            addApp(application);
+
+            application.Run();
+
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Fatal host exception");
+            return 1;
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
+}
