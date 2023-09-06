edc5725615d8f54c943b8f15a40f1bc46ed14914
Andrew McLachlan <andy@mclachlan.family>
2023-09-06 21:46:12 +1000

    Expanded support for FindAsync.

---
+++ src/Asm.Domain.Infrastructure/IQueryableExtensions.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Asm.Domain.Infrastructure;
+public static class IQueryableExtensions
+{
+    public static Task<TEntity?> FindAsync<TEntity, TKey>(this IQueryable<TEntity> queryable, TKey id, CancellationToken cancellationToken = default) where TEntity : KeyedEntity<TKey> =>
+        queryable.SingleOrDefaultAsync(e => e.Id != null && e.Id.Equals(id), cancellationToken);
+}
