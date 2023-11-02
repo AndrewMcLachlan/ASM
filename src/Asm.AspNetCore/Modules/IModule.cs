41682dea58b5f67507cc156abca1a9ab925154f1
Andrew McLachlan <andy@mclachlan.family>
2023-11-03 08:16:08 +1000

    Added module support to AspNetCore.

---
+++ src/Asm.AspNetCore/Modules/IModule.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Asm.AspNetCore.Modules;
+
+/// <summary>
+/// A module is a collection of services and endpoints that can be added to an application.
+/// </summary>
+public interface IModule
+{
+    public IServiceCollection AddServices(IServiceCollection services);
+
+    public IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpoints);
+}
---
+++ src/Asm.AspNetCore/Modules/Modules.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Asm.AspNetCore.Modules;
+
+public static class Modules
+{
+    private static List<IModule> _modules = new();
+
+    public static WebApplicationBuilder RegisterModules(this WebApplicationBuilder builder, Func<IEnumerable<IModule>> modules)
+    {
+        _modules = modules().ToList();
+        builder.Services.AddModules();
+        return builder;
+    }
+
+    public static WebApplicationBuilder RegisterModules(this WebApplicationBuilder builder, Assembly modulseAssembly)
+    {
+        _modules = modulseAssembly.GetTypes().Where(t => t.IsClass && !t.IsAbstract && t.IsAssignableTo(typeof(IModule)))
+                                            .Select(Activator.CreateInstance).Cast<IModule>()
+                                            .ToList();
+        builder.Services.AddModules();
+        return builder;
+    }
+
+    public static IEndpointRouteBuilder MapModuleEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        foreach (var module in _modules)
+        {
+            module.MapEndpoints(endpoints);
+        }
+
+        return endpoints;
+    }
+
+    private static IServiceCollection AddModules(this IServiceCollection services)
+    {
+        foreach (var module in _modules)
+        {
+            module.AddServices(services);
+        }
+
+        return services;
+    }
+}
