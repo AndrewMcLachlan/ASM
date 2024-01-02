8437dcf19c3915e1ca9fc450e0d2f171679bea7e
Andrew McLachlan <andy@mclachlan.family>
2024-01-02 21:48:37 +1000

    Added specify extension.

---
+++ src/Asm.Domain/IQueryableExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace Asm.Domain;
+public static class IQueryableExtensions
+{
+    public static IQueryable<T> Specify<T>(this IQueryable<T> query, ISpecification<T> specification) where T : Entity
+        => specification == null ? query : specification.Apply(query);
+}
