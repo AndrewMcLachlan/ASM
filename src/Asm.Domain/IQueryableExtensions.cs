3c3c28d5b8fca2ebd4e2b76d0de4ef2765b4a6d4
Andrew McLachlan <andy@mclachlan.family>
2024-01-06 15:50:03 +1000

    Added comments to IQueryableExtensions.

--- src/Asm.Domain/IQueryableExtensions.cs
+++ src/Asm.Domain/IQueryableExtensions.cs
@@ -1,6 +1,17 @@
 ﻿namespace Asm.Domain;
+
+/// <summary>
+/// Extensions for the <see cref="IQueryable{T}"/> interface.
+/// </summary>
 public static class IQueryableExtensions
 {
+    /// <summary>
+    /// Apply a specification to a query.
+    /// </summary>
+    /// <typeparam name="T">The type being queried.</typeparam>
+    /// <param name="query">The queryable that this method extends.</param>
+    /// <param name="specification">The specification to be applied.</param>
+    /// <returns>The <paramref name="query"/> with the specification applied.</returns>
     public static IQueryable<T> Specify<T>(this IQueryable<T> query, ISpecification<T> specification) where T : Entity
         => specification == null ? query : specification.Apply(query);
 }
