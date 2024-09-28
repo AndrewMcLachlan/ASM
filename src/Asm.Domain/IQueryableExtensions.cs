1cff56e9f022f0817a108dac838e7ba944c56326
Andrew McLachlan <AndrewMcLachlan@users.noreply.github.com>
2024-09-28 23:30:09 +1000

    Apply specifications from type arguments.

--- src/Asm.Domain/IQueryableExtensions.cs
+++ src/Asm.Domain/IQueryableExtensions.cs
@@ -16,4 +16,15 @@ public static class IQueryableExtensions
     /// <returns>The <paramref name="query"/> with the specification applied.</returns>
     public static IQueryable<T> Specify<T>(this IQueryable<T> query, ISpecification<T> specification) where T : Entity
         => specification == null ? query : specification.Apply(query);
+
+    /// <summary>
+    /// Apply a specification to a query.
+    /// </summary>
+    /// <typeparam name="TEntity">The type being queried.</typeparam>
+    /// <typeparam name="TSpecification">The specification to be applied.</typeparam>
+    /// <param name="query">The queryable that this method extends.</param>
+    /// <returns>The <paramref name="query"/> with the specification applied.</returns>
+    public static IQueryable<TEntity> Specify<TEntity, TSpecification>(this IQueryable<TEntity> query) where TEntity : Entity
+        where TSpecification : ISpecification<TEntity>, new()
+        => new TSpecification().Apply(query);
 }
