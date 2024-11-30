49ad80d37e1782e9c7eafdf5a6c7312745ab8b1f
Andrew McLachlan <AndrewMcLachlan@users.noreply.github.com>
2024-11-30 21:34:49 +1000

    Added OneOf requirement handler.

---
+++ src/Asm.AspNetCore/Authorisation/OneOfAuthorisationRequirement.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Asm.AspNetCore.Authorisation;
+
+/// <summary>
+/// Represents an authorisation requirement that requires at least one of the specified requirements to be met.
+/// </summary>
+/// <param name="requirements">The requirements to check.</param>
+public class OneOfAuthorisationRequirement(params IAuthorizationRequirement[] requirements) : IAuthorizationRequirement
+{
+    /// <summary>
+    /// Gets the requirements to check.
+    /// </summary>
+    public IEnumerable<IAuthorizationRequirement> Requirements { get; } = requirements;
+}
---
+++ src/Asm.AspNetCore/Authorisation/OneOfAuthorisationRequirementHandler.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Asm.AspNetCore.Authorisation;
+
+/// <summary>
+/// Route parameter authorisation handler.
+/// </summary>
+/// <param name="authorisationService">An <see cref="IAuthorizationService"/> instance.</param>
+public abstract class OneOfAuthorisationRequirementHandler(IAuthorizationService authorisationService) : AuthorizationHandler<OneOfAuthorisationRequirement>
+{
+    /// <inheritdoc />
+    protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, OneOfAuthorisationRequirement requirement)
+    {
+        foreach (var subRequirement in requirement.Requirements)
+        {
+            var result = await authorisationService.AuthorizeAsync(context.User, null, subRequirement);
+
+            if (result.Succeeded)
+            {
+                context.Succeed(requirement);
+                return;
+            }
+        }
+
+        context.Fail();
+    }
+
+    /// <summary>
+    /// Performs authorisation based on the route parameter value.
+    /// </summary>
+    protected abstract ValueTask<bool> IsAuthorised(object value);
+
+}
