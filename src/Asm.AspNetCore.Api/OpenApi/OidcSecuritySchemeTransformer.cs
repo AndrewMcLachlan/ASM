ef139ac8e4cf08e013f30084871926c06534a4bf
Andrew McLachlan <andy@mclachlan.family>
2025-11-15 12:34:42 +1000

    Backpedalled to support .NET 9 and 10.

--- src/Asm.AspNetCore.Api/OpenApi/OidcSecuritySchemeTransformer.cs
+++ src/Asm.AspNetCore.Api/OpenApi/OidcSecuritySchemeTransformer.cs
@@ -6,6 +6,7 @@ using Microsoft.OpenApi;
 
 namespace Asm.AspNetCore.Api;
 
+#if NET10_0
 /// <summary>
 /// Adds the OIDC security scheme to the Open API document.
 /// </summary>
@@ -49,3 +50,50 @@ public sealed class OidcSecuritySchemeTransformer(IAuthenticationSchemeProvider 
         }
     }
 }
+#endif
+
+#if NET9_0
+using Microsoft.OpenApi.Models;
+
+/// <summary>
+/// Adds the OIDC security scheme to the Open API document.
+/// </summary>
+public sealed class OidcSecuritySchemeTransformer(IAuthenticationSchemeProvider authenticationSchemeProvider, IOptions<OAuthOptions> oAuthOptions) : IOpenApiDocumentTransformer
+{
+    /// <inheritdoc />
+    public async Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
+    {
+        if (String.IsNullOrEmpty(oAuthOptions.Value.Authority))
+        {
+            return;
+        }
+
+        var authenticationSchemes = await authenticationSchemeProvider.GetAllSchemesAsync();
+        if (authenticationSchemes.Any(authScheme => authScheme.Name == "Bearer"))
+        {
+            var requirements = new Dictionary<string, OpenApiSecurityScheme>
+            {
+                ["oidc"] = new OpenApiSecurityScheme
+                {
+                    Name = "oidc",
+                    Description = "OIDC",
+                    Type = SecuritySchemeType.OpenIdConnect,
+                    OpenIdConnectUrl = new Uri($"{oAuthOptions.Value.Authority}/.well-known/openid-configuration"),
+                    Scheme = "bearer", // "bearer" refers to the header name here
+                    In = ParameterLocation.Header,
+                    BearerFormat = "Json Web Token",
+                }
+            };
+            document.Components ??= new OpenApiComponents();
+            document.Components.SecuritySchemes = requirements;
+            document.SecurityRequirements.Add(new OpenApiSecurityRequirement()
+            {
+                { new OpenApiSecurityScheme()
+                {
+                    Reference = new OpenApiReference() { Id = "oidc", Type = ReferenceType.SecurityScheme },
+                }, new List<string>() },
+            });
+        }
+    }
+}
+#endif
