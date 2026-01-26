63fb9e89478698d20909fb76c4cf15ae606f8b49
Andrew McLachlan <andy@mclachlan.family>
2026-01-26 13:43:30 +1000

    Added unit tests for time extensions.

--- src/Asm/Extensions/System.TimeOnly.cs
+++ src/Asm/Extensions/System.TimeOnly.cs
@@ -1,4 +1,4 @@
-﻿namespace Asm.Extensions;
+namespace System;
 
 /// <summary>
 /// Extension methods for <see cref="TimeOnly"/>.
@@ -8,15 +8,15 @@ public static class TimeOnlyExtensions
     /// <summary>
     /// Static extensions for <see cref="TimeOnly"/>.
     /// </summary>
-    extension(TimeOnly)
+    extension(TimeOnly time)
     {
         /// <summary>
-        /// Gets the current date.
+        /// Gets the current time.
         /// </summary>
         public static TimeOnly Now => TimeOnly.FromDateTime(DateTime.Now);
 
         /// <summary>
-        /// Gets the current date.
+        /// Gets the current UTC time.
         /// </summary>
         public static TimeOnly UtcNow => TimeOnly.FromDateTime(DateTime.UtcNow);
     }
